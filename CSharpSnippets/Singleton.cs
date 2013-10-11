41d9e65ba427a68e1b23a7169e1b0213f47b4f08
Travis Silvers <firestrand@gmail.com>
2013-10-11 11:41:13 -0400

    Added Singleton snippet

---
+++ CSharpSnippets/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpSnippets
+{
+    public sealed class Singleton
+    {
+        private static volatile Singleton _instance;
+        private static readonly object SyncRoot = new object();
+
+        private Singleton(){}
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
